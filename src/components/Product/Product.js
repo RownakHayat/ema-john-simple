2f6b4c4311a2f40d98fa9a1564f3793e5565b38c
unknown <rownakhayat@gmail.com>
2023-02-05 09:21:18 +0600

    add

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,10 +3,20 @@ import './Product.css'
 const Product = (props) => {
 // console.log(props.product);
 const {name, img, seller, price, ratings} = props.product;
+    // console.log(props);
+    
     return (
         <div className='product'>
             <img src={img} alt=""></img>
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={()=> props.handleAddToCart(props.product)} className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
